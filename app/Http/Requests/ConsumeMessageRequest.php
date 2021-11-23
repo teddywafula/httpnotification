ba1a4df5c60349637a96a93ef5d5860a976fc1df
teddywafula <teddywafula@gmail.com>
2021-11-23 22:19:23 +0300

    add requests

---
+++ app/Http/Requests/ConsumeMessageRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class ConsumeMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+      return [
+          "url"=>"url",
+      ];
+    }
+
+    public function messages()
+    {
+      return [
+          "url.url" => __('messages.malformedUrl'),
+      ];
+
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(response()->json($errors
+            , JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+
+}
