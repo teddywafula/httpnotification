7d8cb2bb8cf6703bb14ace1168dbf92d8a6b166b
teddywafula <teddywafula@gmail.com>
2021-11-23 22:10:12 +0300

    update app providers

--- config/app.php
+++ config/app.php
@@ -174,6 +174,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TopicServiceProvider::class,
+        App\Providers\SubscriberServiceProvider::class,
+        App\Providers\PublisherServiceProvider::class,
 
     ],
 
@@ -229,6 +232,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'TopicFacade' => \App\Facades\TopicFacade::class,
+        'SubscriberFacade' => \App\Facades\SubscriberFacade::class,
+        'PublisherFacade' => \App\Facades\PublisherFacade::class,
 
     ],
 
